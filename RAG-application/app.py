044def53825a638d1385c48114e4c446cbf6df8a
askmeaboutlife <rustudor8@gmail.com>
2025-06-19 14:15:36 -0500

    commit

--- RAG-application/app.py
+++ RAG-application/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import utils as u
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 
 st.set_page_config(page_title="PL/SQL Analyzer", layout="wide")
@@ -37,8 +37,13 @@ if chunks:
 
 db = FAISS.from_texts(st.session_state["chunks"], embedding=OpenAIEmbeddings())
 
+llm_ = ChatOpenAI(
+    model_name="gpt-4o",       # 👈 Change this to the model you want
+    temperature=0.1
+)
+
 qa = RetrievalQA.from_chain_type(
-    llm=ChatOpenAI(),
+    llm=llm_,
     retriever=db.as_retriever(),
     chain_type="map_reduce",
 )
@@ -48,12 +53,13 @@ You are analyzing a PL/SQL procedure. Provide the following:
 1. High-level summary (should be no more than 1-2 sentences)
 2. Input parameters and expected data types
 3. Output results or state changes
-4. Business logic steps including a detailed description of what each function does and how it works
-5. Other PL/SQL objects or DB tables this job depends on
+4. An analysis of the buisness logic. This should be detailed and can be as long as needed
+5. A list of every function, its parameters, and a description of what it does
+6. Other PL/SQL objects or DB tables this job depends on
 
 '''
 if st.button("Analyze PL/SQL Code"):
-    result = qa.run(prompt)
+    result = qa.invoke(prompt)
     # Display the result in an expandable section that can be manually edited
     with st.expander("🔍 Analysis Result"):
-        st.text_area("Analysis Output", value=result, height=300)
+        st.text_area("Analysis Output", value=result, height=500)
